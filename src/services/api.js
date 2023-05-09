82eb8a09f484efa62a89763f76d52a7ef0a75887
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-09 09:53:38 +0300

    DELETE: commits

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 axios.defaults.baseURL = `https://645640f95f9a4f23613ed358.mockapi.io/tweets`;
 
-
 export async function fetchTweets(page) {
   const searchParams = new URLSearchParams({
     limit: 3,
@@ -18,16 +17,3 @@ export async function changeFollowers(id, newAmount) {
   console.log(response)
   return await response;
 }
-// export async function fetchImages (searchQuery, queryPage) {
-//   const searchParams = new URLSearchParams({
-//     q: searchQuery,
-//     page: queryPage,
-//     key: KEY,
-//     image_type: 'photo',
-//     orientation: 'orientation',
-//     per_page: 12,
-//   });
-
-//   const response = await axios.get(`${ENDPOINT}${searchParams}`);
-//   return await response.data;
-//   }
